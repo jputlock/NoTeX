b49ef16cdd1bb61cf5980a7c0d84278e58e3d901
jputlock <jeffreyputlock62@gmail.com>
2019-07-14 02:20:36 -0400

    Started working on ModuleList as primary DS and added container files. Rearranged editor files into GUI folder. Made gitignore more streamlined"

---
+++ src/Containers/module.h
@@ -0,0 +1,13 @@
+#pragma once
+
+/** @brief Modules are the largest containers that can hold components and other
+ * modules.
+ *
+ * todo: description here
+ *
+ */
+
+class Module {
+    
+
+};
---
+++ src/ModuleList.h
@@ -0,0 +1,16 @@
+#pragma once
+
+/** @brief Data structure used to hold the entire Module/Component structure.
+ *
+ * todo: description here
+ *
+ */
+class ModuleList {
+private:
+    int x;
+public:
+    // Default Constructor
+    ModuleTree() {
+
+    }
+};
